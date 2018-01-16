3f00768c19ef90c7c5db1bd27773b860a8f6201c
Randy <randy.adamson@gmail.com>
2018-01-16 16:55:09 -0600

    Added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,65 @@
+const {MongoClient, ObjectID} = require('mongodb');  
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err){
+		return console.log('Unable to connect to server');
+	}
+	console.log('Connected to MongoDB server');
+
+	// findOneAndUpdate   -   update a document and get the new document back
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5a5e7c463b68580db70adf5d')
+	// }, {
+	// 	$set: {     //update operator.  https://docs.mongodb.com/manual/reference/operator/update/
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// })
+
+//RESULT
+
+// ⇒  node ./playground/mongodb-update.js
+// Connected to MongoDB server
+// { lastErrorObject: { n: 1, updatedExisting: true },
+//   value:
+//    { _id: 5a5e7c463b68580db70adf5d,
+//      text: 'eat lunch',
+//      completed: true },
+//   ok: 1 }
+
+//CHALLENGE  -  Set name in Users to Randy and increment the age by 1
+
+db.collection('Users').findOneAndUpdate(
+	{name: 'Jeff'}, 
+	{$set: {name: 'Randy'}}, 
+	{returnOriginal: false}
+	).then((result) => {
+		console.log(result);
+	}, (err) => {
+		console.log(err);
+	});
+
+db.collection('Users').findOneAndUpdate(
+	{age: 40},
+	{$inc: {age: 1}},
+	{returnOriginal: false}
+	).then((result) => {
+		console.log(result);
+	}, (err) => {
+		console.log(err);
+	});
+
+
+
+	// db.close();
+});
+
+
+
+
+
+
