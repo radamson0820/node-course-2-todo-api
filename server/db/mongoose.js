8c30b436cb62f9ee9917416ce930b0d225210106
Randy <randy.adamson@gmail.com>
2018-01-22 17:03:15 -0600

    change mongoose connect file to and instead of or

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://todoapp:todoapp@ds111648.mlab.com:11648/todoapp' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://todoapp:todoapp@ds111648.mlab.com:11648/todoapp' && 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose: mongoose  // when someone requests the mongoose let, they will get mongoose back from the library.
