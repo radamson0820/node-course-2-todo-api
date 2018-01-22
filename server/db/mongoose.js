f28b505cb92061301567aa6bfd9941e6c2c421d3
Randy <randy.adamson@gmail.com>
2018-01-22 15:49:20 -0600

    Setup app for Heroku added mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://todoapp:todoapp@ds111648.mlab.com:11648/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose: mongoose  // when someone requests the mongoose let, they will get mongoose back from the library.
